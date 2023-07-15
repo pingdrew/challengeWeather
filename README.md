e8168711b4bb0ae744896dff5b9bbe2424229e90
pingdrew <pingdrew0@gmail.com>
2023-07-14 20:09:06 -0500

    finished

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # challengeWeather
+
+## Description
+Project created for the Coders Bootcamp Pre-Work
+
+motivation for completing this project was for gaining the experience from getting my hands on apis and referencing their data in my webpage so I can use the same skill and apply it to my project.
+## Installation
+
+N/A
+
+## Usage
+
+enter a city name accompanied by a country if you wish and click your history entries to bring those back up. 
+
+## Credits
+
+I got my html foundation from my friend lekendrick but I did most if not all of my js. It was brain breaking but I did it in JQuery too which I am less familliar with
+
+## Tests
+
+Open the page, and change the size of the screen to anything. It should be responsive and still look presentable and clean while giving you data on the cities you want
--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,26 @@
+// waits until the doc is loaded and ready to start the functions and variable assignments
 $(document).ready(function () {
+  var myKey = '16ceabcc9c9a6f135440a6efd1165492'
+  var localStorageCurrent = localStorage.getItem('pastCities');
+  var localCities = [];
+  var localParsed = JSON.parse(localStorage.getItem('pastCities'));
 
-  $('button').on('click', function (event) {
-    event.preventDefault();
+  //this simply appends li's to the ul dynamically based on how many there are saved so I dont have to repeat the code
+  function showHistory() {
+    if (localStorageCurrent) {
+      localParsed.forEach(function (i) {
+        var li = document.createElement('li');
+        li.innerText = i;
+        $('#history').append(li);
+      });
+    };
+  };
 
-    var myKey = '16ceabcc9c9a6f135440a6efd1165492'
+  showHistory();
 
-    var input = $('#username').val().replaceAll(" ", "")
-    $('#username').val().replaceAll("")
-    var localCities = [];
-    localCities.push(localStorage.getItem('pastCities'))
-    localCities.push(input)
-    localStorage.setItem('pastCities', localCities);
-    console.log(localCities);
-
-    var geoURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + input + "&limit=1&appid=" + myKey;
+// this function gets the data you searched and finds it on a geo locater api
+  function fetchData(choice) {
+    var geoURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + choice + "&limit=1&appid=" + myKey;
 
     fetch(geoURL, {
       cache: 'reload',
@@ -22,37 +29,63 @@ $(document).ready(function () {
       return response.json();
 
     }).then(function (data) {
+
+      // then puts the found geolocation into the weather apis so they get results for that place
       var owmURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&appid=' + myKey;
       var todayWeather = 'https://api.openweathermap.org/data/2.5/weather?lat=' + data[0].lat + '&lon=' + data[0].lon + '&units=imperial&appid=' + myKey;
 
       fetch(owmURL).then(function (response) {
         return response.json();
-      }).then(function (data) {
-        console.log(data)
-        // paste here
 
-        $('.5dayFork').each(function(i) {
-          $(this).find('.date').text(data.list[i*8+4].dt_txt.split(' ')[0]);
+      }).then(function (data) {
+        
+        // sets the text of all the attributes below to their corresponding date and time set by the algorithm derrived by me
+        $('.5dayFork').each(function (i) {
+          $(this).find('.date').text(data.list[i * 8 + 4].dt_txt.split(' ')[0]);
           $(this).find('.condition').attr('src', '#');
-          $(this).find('.temp').text('Temperature: ' + data.list[i*8+4].main.temp + 'F');
-          $(this).find('.wind').text('Wind speed: ' + data.list[i*8+4].main.temp + 'mph');
-          $(this).find('.humid').text('Humidity: ' + data.list[i*8+4].main.temp + '%');
+          $(this).find('.temp').text('Temperature: ' + data.list[i * 8 + 4].main.temp + 'F');
+          $(this).find('.wind').text('Wind speed: ' + data.list[i * 8 + 4].wind.speed + 'mph');
+          $(this).find('.humid').text('Humidity: ' + data.list[i * 8 + 4].main.humidity + '%');
         });
       });
 
       fetch(todayWeather).then(function (response) {
         return response.json();
+        // finally sets the same attributes to the today section
       }).then(function (data) {
         console.log(data)
-        // paste here
         $('#today').find('.date').text(data.name + ' (' + dayjs().format('MM/DD/YYYY') + ')');
-        $('#today').find('.condition').text('EMOJI');
+        $('#today').find('.condition').attr('src', '#');
         $('#today').find('.temp').text('Temperature: ' + data.main.temp + ', Feels like: ' + data.main.feels_like);
         $('#today').find('.wind').text('Wind speed: ' + data.wind.speed);
         $('#today').find('.humid').text('Humidity: ' + data.main.humidity);
       });
-
     });
+  };
+
+    //when the button is clicked, we want to save the input, erase the textbox, and store it in localstorage if it follows our rules.
+  $('button').on('click', function (event) {
+    event.preventDefault();
+
+    var input = $('#username').val().replaceAll(" ", "")
+    $('#username').val('');
+
+    if (input) {
+      if (localStorageCurrent && !localParsed.includes(input)) {
+        localParsed.push(input)
+        localStorage.setItem('pastCities', JSON.stringify(localParsed))
+      } else if (!localCities.includes(input)) {
+        localCities.push(input)
+        localStorage.setItem('pastCities', JSON.stringify(localCities))
+      };
+    };
+    //then we show the history and display the data for what you have chosen
+    fetchData(input);
   });
+  
+  // when you click an option on the history list it will run the function that displays the weather with your value
+  $('li').on('click', function () {
+    var choice = $(this).text()
+    fetchData(choice)
+  })
 });
-// http://openweathermap.org/img/w/{image.png}
